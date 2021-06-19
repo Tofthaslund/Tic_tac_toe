a34dfe9a20228cd4df8646a01d7635746eddce67
Lasse Haslund <tofthaslund@gmail.com>
2021-06-19 19:29:27 +0100

    created JS file for gameview

--- js/main.js
+++ js/main.js
@@ -1,12 +1,5 @@
 import Game from "./game.js";
+import GameView from "./gameview.js"
 
 let game = new Game();
 
-game.makeMove(0); //X
-game.makeMove(3); //0
-game.makeMove(7); //X
-game.makeMove(4); //0
-game.makeMove(2); //X
-game.makeMove(5); //0
-console.log(game.board);
-console.log(game.findWinningCombination());
