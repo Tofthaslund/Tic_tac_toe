8dda9a27cb58f5f0cc144cf37b9a45ed8d9a45a4
Lasse Haslund <tofthaslund@gmail.com>
2021-06-19 19:47:51 +0100

    imported Goolge icons for refresh icon

--- js/gameview.js
+++ js/gameview.js
@@ -1,4 +1,4 @@
-dexport default class GameView {
+export default class GameView {
     constructor(root) {
         this.root = root;
         this.root.innerHTML = `
--- js/main.js
+++ js/main.js
@@ -2,4 +2,6 @@ import Game from "./game.js";
 import GameView from "./gameview.js"
 
 let game = new Game();
+let gameview = new GameView(document.getElementById("app"));
+
 
